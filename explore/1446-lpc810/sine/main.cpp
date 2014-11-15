4e8bdb4b566da920fe9a342dff0f62f3648b31db
Jean-Claude Wippler <jc@wippler.nl>
2014-11-15 20:14:23 +0100

    enable PLL and run the clock at 30 MHz

--- explore/1446-lpc810/sine/main.cpp
+++ explore/1446-lpc810/sine/main.cpp
@@ -12,6 +12,28 @@
 int32_t phase;
 volatile int32_t dac;
 
+#define SYSPLLCTRL_Val      0x24
+#define SYSPLLCLKSEL_Val    0
+#define MAINCLKSEL_Val      3
+#define SYSAHBCLKDIV_Val    2
+
+static void setMaxSpeed () {
+    LPC_SYSCON->SYSPLLCLKSEL = SYSPLLCLKSEL_Val;    // select PLL input
+    LPC_SYSCON->SYSPLLCLKUEN = 0x01;                // update clock source
+    while (!(LPC_SYSCON->SYSPLLCLKUEN & 0x1)) ;     // wait until updated
+
+    LPC_SYSCON->SYSPLLCTRL = SYSPLLCTRL_Val;        // main clock is PLL out
+    LPC_SYSCON->PDRUNCFG &= ~(0x1<<7);              // power-up SYSPLL
+    while (!(LPC_SYSCON->SYSPLLSTAT & 0x1)) ;       // wait until PLL locked
+
+    LPC_SYSCON->MAINCLKSEL = MAINCLKSEL_Val;        // select PLL clock output
+    LPC_SYSCON->MAINCLKUEN = 0x01;                  // update MCLK clock source
+    while (!(LPC_SYSCON->MAINCLKUEN & 0x1)) ;       // wait until updated
+
+    LPC_SYSCON->SYSAHBCLKDIV = SYSAHBCLKDIV_Val;
+
+}
+
 static void mrtInit (int count) {
     // enable MRT clock and reset it
     LPC_SYSCON->SYSAHBCLKCTRL |= 1<<10;
@@ -27,12 +49,14 @@ static void mrtInit (int count) {
 }
 
 int main () {
+    setMaxSpeed();
+
     LPC_SYSCON->SYSAHBCLKCTRL |= 1<<6;  // enable GPIO and IOCON
     LPC_SYSCON->PRESETCTRL |= 1<<10;
     LPC_SWM->PINENABLE0 |= 1<<2;        // disable SWCLK
     LPC_GPIO_PORT->DIR0 |= 1<<3;
 
-    mrtInit(12000000 / (50 * 1024));    // output 50 Hz of 1024 samples each
+    mrtInit(30000000 / (50 * 1024));    // output 50 Hz of 1024 samples each
 
     // use masked pin access to make the following loop as fast as possible
     LPC_GPIO_PORT->MASK0 = ~(1<<3);
@@ -45,8 +69,6 @@ int main () {
 }
 
 extern "C" void NMI_Handler () {
-    //LPC_GPIO_PORT->DIR0 &= ~(1<<3);
-
     LPC_MRT->Channel[0].STAT = 1<<0; // clear interrupt
 
     uint8_t off = ++phase;
@@ -59,6 +81,4 @@ extern "C" void NMI_Handler () {
         dac = (1<<15) - ampl;
     else
         dac = (1<<15) + ampl;
-
-    //LPC_GPIO_PORT->DIR0 |= 1<<3;
 }
