fdf793ce1685f8cd2256d4f790e2ee1feb98c94f
Jean-Claude Wippler <jc@wippler.nl>
2014-12-02 00:46:55 +0100

    simplify

--- explore/1446-lpc810/toggle/main.cpp
+++ explore/1446-lpc810/toggle/main.cpp
@@ -33,8 +33,6 @@ int main () {
     //LPC_FLASHCTRL->FLASHCFG = 0;          // 1 flash clock instead of 2
 
     LPC_SWM->PINENABLE0 |= (1<<2) | (1<<3); // disable SWCLK and SWDIO
-    LPC_SYSCON->SYSAHBCLKCTRL |= 1<<6;      // enable GPIO
-    LPC_SYSCON->PRESETCTRL |= 1<<10;        // reset GPIO block
     LPC_GPIO_PORT->DIR0 |= (1<<2) | (1<<3); // set PIO0_2 & PIO0_3 as outputs
 
     SysTick_Config(12000000/50000);         // 50 KHz interrupt rate
