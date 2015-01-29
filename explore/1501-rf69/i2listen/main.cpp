4dc03d39a09b31912d10f1f422e860807afb26ef
Jean-Claude Wippler <jc@wippler.nl>
2015-01-29 12:23:17 +0100

    cleanup

--- explore/1501-rf69/i2listen/main.cpp
+++ explore/1501-rf69/i2listen/main.cpp
@@ -14,17 +14,20 @@
 RF69<SpiDevice> rf;
 uint8_t rxBuf[66];
 
-typedef struct {
+struct Payload {
     uint8_t seq, len, rssi, lna;
     uint16_t afc;
     uint8_t buf[66];
-} Payload;
+} out;                      // this is the data as returned over I2C
 
-Payload out;
+uint32_t i2cBuffer [24];    // data area used by ROM-based I2C driver
+I2C_HANDLE_T* ih;           // opaque handle used by ROM-based I2C driver
+I2C_PARAM_T i2cParam;       // input parameters for pending I2C request
+I2C_RESULT_T i2cResult;     // return values for pending I2C request
 
-uint32_t i2cBuffer [24];
-I2C_HANDLE_T* ih;
-uint8_t regNum;
+uint8_t i2cRecvBuf [2];     // receive buffer: address + register number
+
+void i2cSetupRecv (), i2cSetupSend (int); // forward
 
 void i2cSetup () {
     for (int i = 0; i < 3000000; ++i) __ASM("");
@@ -44,24 +47,21 @@ extern "C" void I2C0_IRQHandler () {
     LPC_I2CD_API->i2c_isr_handler(ih);
 }
 
-I2C_PARAM_T i2cParam;
-I2C_RESULT_T i2cResult;
-uint8_t i2cRecvBuf [2];
-
-void i2cSetupRecv (); // forward
-void i2cSetupSend (); // forward
-
+// called when I2C reception has been completed
 void i2cRecvDone (uint32_t err, uint32_t) {
+    i2cSetupRecv();
     if (err == 0)
-        regNum = i2cRecvBuf[1];
-    i2cSetupRecv();
-    i2cSetupSend();
+        i2cSetupSend(i2cRecvBuf[1]);
 }
 
-void i2cSendDone (uint32_t, uint32_t) {
+// called when I2C transmission has been completed
+void i2cSendDone (uint32_t err, uint32_t) {
+    if (err == 0)
+        out.len = 0;
     i2cSetupRecv();
 }
 
+// prepare to receive the register number
 void i2cSetupRecv () {
     i2cParam.func_pt = i2cRecvDone;
     i2cParam.num_bytes_send = 0;
@@ -70,14 +70,15 @@ void i2cSetupRecv () {
     LPC_I2CD_API->i2c_slave_receive_intr(ih, &i2cParam, &i2cResult);
 }
 
-void i2cSetupSend () {
+// prepare to transmit either the byte count or the actual data
+void i2cSetupSend (int regNum) {
     i2cParam.func_pt = i2cSendDone;
     i2cParam.num_bytes_rec = 0;
     if (regNum == 0) {
         i2cParam.num_bytes_send = 1;
         i2cParam.buffer_ptr_send = &out.len;
     } else {
-        i2cParam.num_bytes_send = out.len + 6;
+        i2cParam.num_bytes_send = out.len;
         i2cParam.buffer_ptr_send = (uint8_t*) &out;
     }
     LPC_I2CD_API->i2c_slave_transmit_intr(ih, &i2cParam, &i2cResult);
@@ -97,7 +98,7 @@ int main () {
         int len = rf.receive(rxBuf, sizeof rxBuf);
         if (len >= 0) {
             ++out.seq;
-            out.len = len;
+            out.len = len + 6;
             out.rssi = rf.rssi;
             out.lna = rf.lna;
             out.afc = rf.afc;
