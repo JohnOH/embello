7e3bace76d3a4bb99161779c7cf919fa7df6fe13
Jean-Claude Wippler <jc@wippler.nl>
2015-03-29 18:33:01 +0200

    better names and logic

--- explore/1514-lpc/eeprom/main.cpp
+++ explore/1514-lpc/eeprom/main.cpp
@@ -5,49 +5,68 @@
 
 class Flash {
 public:
-  Flash (int size) : bytes (size) {}
+  Flash () {}
 
-  const void* base (int offset) const {
-    return (const void*) (0x1000 - (2 - offset) * bytes);
+  void erase (int pos, int num) {
+    printf("erase(%d,%d)\n", pos, num);
+    // TODO erase flash
   }
 
-  void save (const void* ptr, int offset) {
+  void save (int pos, int num, const void* ptr) {
+    printf("save(%d,%d)\n", pos, num);
     // TODO save to flash
   }
-
-private:
-  int bytes; // TODO could be passed in as template constant
 };
 
 template < typename FLASH, int NUM >
-class Eeprom {
-  enum { S_EMPTY = 0xFFFFFFFF, S_ACTIVE = 0x12345678, S_STABLE = 0x00000000 };
+class RomVars {
+  enum { S_EMPTY = 0xFFFF, S_ACTIVE = 0x1234, S_STABLE = 0x0000 };
   typedef struct { uint16_t key, value; } Tuple;
 
-  void clear () {
-    memset(map, 0, sizeof map);
-    memset(data, ~0, sizeof data);
+  const void* base (int offset) const {
+    return (const void*) (0x1000 - (2 - offset) * NUM * sizeof (Tuple));
   }
 
-  void collect (int offset) {
-    const Tuple* orig = (const Tuple*) flash.base(offset);
-    for (int i = 1; i < NUM; ++i) {
-      int m = orig[i].key;
-      if (m >= NUM)
-        break;
-      if (map[m] == 0)
-        map[m] = ++fill;
-      data[map[m]] = orig[i];
-    }
+  void pruneAndSave () {
+    fill = 0;
+    memset(map, 0, sizeof map);
+    memset(data, ~0, sizeof data);
+
+    const Tuple* orig = (const Tuple*) base(1);
+    if (orig[0].key == S_STABLE)
+      for (int i = 1; i < NUM; ++i) {
+        int m = orig[i].key;
+        if (m >= NUM)
+          break;
+        if (map[m] == 0) {
+          if (orig[i].value == 0xFFFF)
+            continue; // no need to use an entry for storing the default
+          map[m] = ++fill;
+        }
+        data[map[m]] = orig[i];
+      }
+
+    eraseAndSave(0);
+  }
+
+  void eraseAndSave (int index) {
+    flash.erase(48 + index, 1);
+    data[0].key = S_ACTIVE;
+    flash.save(48 + index, 1, data);
+    data[0].key = S_STABLE;
+    flash.save(48 + index, 1, data);
   }
 
 public:
-  Eeprom () : fill (0), flash (NUM * sizeof (Tuple)) {
-    clear();
-
-    const Tuple* orig = (const Tuple*) flash.base(0);
-    if (orig[0].value == S_STABLE)
-      collect(0);
+  void init () {
+    const Tuple* orig = (const Tuple*) base(0);
+    if (orig[0].value == S_STABLE) {
+      memcpy(data, orig, sizeof data);
+      for (fill = 1; fill < NUM; ++fill)
+        if (data[fill].key > NUM)
+          break;
+    } else
+      pruneAndSave();
   }
 
   uint16_t operator[] (int index) const {
@@ -59,14 +78,16 @@ public:
     if (value == data[map[index]].value)
       return;
     if (fill >= NUM) {
-      flash.save(data, 1);
-      clear();
-      collect(1);
+      eraseAndSave(1);
+      pruneAndSave();
     }
     int m = map[index] = ++fill;
     printf("set(%d,%u) -> slot %d\n", index, value, m);
-    data[m].key = index;
-    data[m].value = value;
+    if (m < NUM) {
+      data[m].key = index;
+      data[m].value = value;
+      flash.save(48, 1, data);
+    }
   }
 
 private:
@@ -75,20 +96,21 @@ private:
   FLASH flash;
 };
 
-Eeprom<Flash,16> eeprom;
+RomVars<Flash,16> rom;
 
 int main () {
   tick.init(1000);
   serial.init(115200);
+  rom.init();
 
-  printf("\n[hello]\n");
+  printf("\n[eeprom]\n");
 
   unsigned i = 0;
   while (true) {
     tick.delay(500);
     int n = ++i % 7;
-    printf("%u\n", eeprom[n]);
-    eeprom.set(n, tick.millis);
+    printf("%u\n", rom[n]);
+    rom.set(n, tick.millis);
   }
 }
 
