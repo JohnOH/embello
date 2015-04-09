22c1b2dda0a47a9b826de5d30728c234e2f87019
Jean-Claude Wippler <jc@wippler.nl>
2015-04-09 12:58:12 +0200

    add operators and rf driver

--- explore/1514-lpc/cmder/main.cpp
+++ explore/1514-lpc/cmder/main.cpp
@@ -1,8 +1,21 @@
 // Command line interface using the serial port.
 
+#define WITH_TEST 1
+#define WITH_RF69 1
+
 #include "sys.h"
-#include "uart_irq.h"
 #include <string.h>
+
+#include "uart_irq.h"
+
+#if WITH_RF69
+#define chThdYield() // FIXME still used in rf69.h
+#include "spi.h"
+#include "rf69.h"
+
+RF69<SpiDev0> rf;
+uint8_t rfBuf[66], myNodeId;
+#endif
 
 namespace Cmder {
   typedef void (*Cmd)();
@@ -30,9 +43,21 @@ namespace Cmder {
     printf("\n");
   }
 
-  void cmd_ram_plus () {
-    top += 0x10000000;
-  }
+  void cmd_add () { top += pop(); }
+  void cmd_sub () { top = pop() - top; }
+  void cmd_mul () { top *= pop(); }
+  void cmd_div () { top = pop() / top; }
+  void cmd_mod () { top = pop() % top; }
+  void cmd_negate () { top = -top; }
+
+  void cmd_invert () { top = ~top; }
+  void cmd_and () { top &= pop(); }
+  void cmd_or () { top |= pop(); }
+  void cmd_xor () { top ^= pop(); }
+  void cmd_lshift () { top = pop() << top; }
+  void cmd_rshift () { top = (unsigned) pop() >> top; }
+
+  void cmd_ram_plus () { top += 0x10000000; }
 
   void cmd_dump () {
     int count = pop();
@@ -63,10 +88,44 @@ namespace Cmder {
     }
   }
 
+#if WITH_RF69
+  void cmd_rf_init () {
+    int freq = pop();
+    int group = pop();
+    myNodeId = pop();
+    rf.init(myNodeId, group, freq);
+  }
+
+  void cmd_rf_txpower () {
+    rf.txPower(pop()); // 0 = min .. 31 = max
+  }
+#endif
+
   const Def commands [] = {
     { "nl", cmd_nl },
+
+    { "add", cmd_add },
+    { "sub", cmd_sub },
+    { "mul", cmd_mul },
+    { "div", cmd_div },
+    { "mod", cmd_mod },
+    { "negate", cmd_negate },
+
+    { "invert", cmd_invert },
+    { "and", cmd_and },
+    { "or", cmd_or },
+    { "xor", cmd_xor },
+    { "lshift", cmd_lshift },
+    { "rshift", cmd_rshift },
+
     { "ram+", cmd_ram_plus },
     { "dump", cmd_dump },
+
+#if WITH_RF69
+    { "rf-init", cmd_rf_init },
+    { "rf-txpower", cmd_rf_txpower },
+#endif
+
     { 0, 0 }
   };
 }
@@ -76,7 +135,17 @@ int main () {
   serial.init(115200);
 
   printf("\n[cmder]\n");
+
+#if WITH_RF69
+  // jnp v0.2
+  LPC_SWM->PINASSIGN[3] = 0x06FFFFFF;
+  LPC_SWM->PINASSIGN[4] = 0xFF080B09;
+  LPC_IOCON->PIO0[IOCON_PIO11] |= (1<<8); // std GPIO, not I2C pin
+  // LPC_IOCON->PIO0[IOCON_PIO10] |= (1<<8); // std GPIO, not I2C pin
+  //rf.init(63, 255, 999); // nonsense values, won't receive anything
+#endif
   
+#if WITH_TEST
   Cmder::push(0x0000);
   Cmder::push(2);
   Cmder::lookup("dump")();
@@ -90,10 +159,22 @@ int main () {
   Cmder::push(8);
   Cmder::lookup("dump")();
   // 0 2 dump nl  3 2 dump nl  0 ram+ 8 dump
+#endif
 
   while (true) {
     int ch = uart0RecvChar();
     if (ch >= 0)
       printf("%d\n", ch);
+#if WITH_RF69
+    if (myNodeId != 0) {
+      int len = rf.receive(rfBuf, sizeof rfBuf);
+      if (len >= 0) {
+        printf("RF ");
+        for (int i = 0; i < len; ++i)
+          printf("%02x", rfBuf[i]);
+        printf(" (%d%s%d:%d)\n", rf.rssi, rf.afc < 0 ? "" : "+", rf.afc, rf.lna);
+      }
+    }
+#endif
   }
 }
