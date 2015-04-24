cd26ae8ad493ec4d26906cc6a7dde4c7ec064814
Jean-Claude Wippler <jc@wippler.nl>
2015-04-24 18:09:54 +0200

    comment tweaks

--- explore/1514-lpc/button/main.cpp
+++ explore/1514-lpc/button/main.cpp
@@ -5,7 +5,7 @@
 volatile bool triggered;
 
 extern "C" void PIN_INT0_IRQHandler () {
-  LPC_PININT->IST = (1<<0); // clear interrupt
+  LPC_PININT->IST = (1<<0);       // clear interrupt
   triggered = true;
 }
 
@@ -15,10 +15,10 @@ int main () {
 
   printf("\n[button]\n");
 
-  LPC_SWM->PINENABLE0 |= (3<<2); // disable SWCLK/SWDIO
+  LPC_SWM->PINENABLE0 |= (3<<2);  // disable SWCLK/SWDIO
   // PIO0_2 is already an input with pull-up enabled
-  LPC_SYSCTL->PINTSEL[0] = 2; // pin 2 triggers pinint 0
-  LPC_PININT->SIENF = (1<<0); // enable falling edge
+  LPC_SYSCTL->PINTSEL[0] = 2;     // pin 2 triggers pinint 0
+  LPC_PININT->SIENF = (1<<0);     // enable falling edge
   NVIC_EnableIRQ(PININT0_IRQn);
 
   while (true) {
