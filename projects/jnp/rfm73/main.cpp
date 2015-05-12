c6b181ec9da5d5773e7e7c669609b7d348636691
Jean-Claude Wippler <jc@wippler.nl>
2015-05-12 11:32:35 +0200

    tweaks, still not working

--- projects/jnp/rfm73/main.cpp
+++ projects/jnp/rfm73/main.cpp
@@ -6,14 +6,13 @@
 #define RFM73 0 // 0 = RFM70, 1 = RFM73
 #include "rf73.h"
 
-RF73<SpiDev0,7> rf;
+RF73<SpiDev0,15> rf;
 
 int main () {
   serial.init(115200);
+  printf("\n[rfm73] %d\n", RFM73);
 
-  printf("\n[rfm73]\n");
-
-  // j169: sck 17, ssel 23, miso 9, mosi 8, sel 1
+  // j169: sck 17, ssel 23, miso 9, mosi 8, ce 15
   LPC_SWM->PINASSIGN[3] = 0x11FFFFFF;
   LPC_SWM->PINASSIGN[4] = 0xFF170908;
 
@@ -26,14 +25,14 @@ int main () {
 
   while (true) {
     if (++cnt == 0) {
-        int txLen = ++txBuf[0] % 64;
+        int txLen = ++txBuf[0] % RF73_MAXLEN;
         printf(" > #%d, %db\n", txBuf[0], txLen);
         rf.send(0, txBuf, txLen);
     }
 
     uint8_t rxBuf [RF73_MAXLEN];
     int len = rf.receive(rxBuf, sizeof rxBuf);
-    if (len >= 0) {
+    if (len > 0) {
       printf("OK ");
       for (int i = 0; i < len; ++i)
           printf("%02x", rxBuf[i]);
--- projects/jnp/rfm73/rf73.h
+++ projects/jnp/rfm73/rf73.h
@@ -94,75 +94,75 @@ const uint8_t bank1_init [] = {
 #else
   4, 13, 0x46,0xB4,0x80,0x00,
 #endif
-	11, 14, 0x41,0x20,0x08,0x04,0x81,0x20,0xCF,0xF7,0xFE,0xFF,0xFF,
+  11, 14, 0x41,0x20,0x08,0x04,0x81,0x20,0xCF,0xF7,0xFE,0xFF,0xFF,
   0
 };
 
 template< typename SPI, int SELPIN >
 class RF73 {
-public:
-  void init (uint8_t chan);
+  public:
+    void init (uint8_t chan);
 
-  int receive (void* ptr, int len);
-  void send (uint8_t ack, const void* ptr, int len);
+    int receive (void* ptr, int len);
+    void send (uint8_t ack, const void* ptr, int len);
 
-private:
-  void configure (const uint8_t* p);
+  private:
+    void configure (const uint8_t* p);
 
-  void select () {
-    LPC_GPIO_PORT->CLR[0] = 1<<SELPIN; // TODO this is architecture-specific
-  }
+    void select () {
+      LPC_GPIO_PORT->B[0][SELPIN] = 0; // TODO this is architecture-specific
+    }
 
-  void deselect () {
-    LPC_GPIO_PORT->SET[0] = 1<<SELPIN; // TODO this is architecture-specific
-  }
+    void deselect () {
+      LPC_GPIO_PORT->B[0][SELPIN] = 1; // TODO this is architecture-specific
+    }
 
-  uint8_t readReg (uint8_t addr) {
-    return spi.rwReg(addr, 0);
-  }
+    uint8_t readReg (uint8_t addr) {
+      return spi.rwReg(addr, 0);
+    }
 
-  void writeReg (uint8_t addr, uint8_t val) {
-    if (addr < 32)
-      addr |= RF_WRITE_REG;
-    spi.rwReg(addr, val);
-  }
+    void writeReg (uint8_t addr, uint8_t val) {
+      if (addr < 32)
+        addr |= RF_WRITE_REG;
+      spi.rwReg(addr, val);
+    }
 
-  void readBuf (uint8_t addr, uint8_t *pBuf, uint8_t length) {
-  	spi.enable();
-  	spi.transfer(addr);
-  	for (uint8_t i = 0; i < length; ++i)
-  		pBuf[i] = spi.transfer(0);
-  	spi.disable();
-  }
+    void readBuf (uint8_t addr, uint8_t *pBuf, uint8_t length) {
+      spi.enable();
+      spi.transfer(addr);
+      for (uint8_t i = 0; i < length; ++i)
+        pBuf[i] = spi.transfer(0);
+      spi.disable();
+    }
 
-  void writeBuf (uint8_t addr, const uint8_t *pBuf, uint8_t length) {
-    spi.enable();
-    if (addr < 32)
-      addr |= RF_WRITE_REG;
-  	spi.transfer(addr);
-  	for (uint8_t i = 0; i < length; ++i)
-      spi.transfer(pBuf[i]);
-    spi.disable();
-  }
+    void writeBuf (uint8_t addr, const uint8_t *pBuf, uint8_t length) {
+      spi.enable();
+      if (addr < 32)
+        addr |= RF_WRITE_REG;
+      spi.transfer(addr);
+      for (uint8_t i = 0; i < length; ++i)
+        spi.transfer(pBuf[i]);
+      spi.disable();
+    }
 
-  void rxMode () {
-    select();
-  	writeReg(CONFIG, readReg(CONFIG) | 1);
-    deselect();
-  }
+    void rxMode () {
+      select();
+      writeReg(CONFIG, readReg(CONFIG) | 1);
+      deselect();
+    }
 
-  void txMode () {
-    select();
-  	writeReg(CONFIG, readReg(CONFIG) & ~1);
-    deselect();
-  }
+    void txMode () {
+      select();
+      writeReg(CONFIG, readReg(CONFIG) & ~1);
+      deselect();
+    }
 
-  void setBank (char bank) {
-  	if (bank != (readReg(7) >> 7))
-  		writeReg(ACTIVATE_CMD, 0x53);
-  }
+    void setBank (char bank) {
+      if (bank != (readReg(7) >> 7))
+        writeReg(ACTIVATE_CMD, 0x53);
+    }
 
-  SPI spi;
+    SPI spi;
 };
 
 template< typename SPI, int SELPIN >
@@ -171,18 +171,18 @@ void RF73<SPI,SELPIN>::init (uint8_t chan) {
   deselect();
   spi.master(2);
 
-	setBank(0);
-	if (readReg(29) == 0)
-		writeReg(ACTIVATE_CMD, 0x73);
+  setBank(0);
+  if (readReg(29) == 0)
+    writeReg(ACTIVATE_CMD, 0x73);
 
   configure(bank0_init);
-	writeReg(5, chan);
+  writeReg(5, chan);
 
-	setBank(1);
+  setBank(1);
   configure(bank1_init);
 
-	setBank(0);
-	rxMode();
+  setBank(0);
+  rxMode();
 }
 
 template< typename SPI, int SELPIN >
@@ -197,12 +197,12 @@ void RF73<SPI,SELPIN>::configure (const uint8_t* data) {
 
 template< typename SPI, int SELPIN >
 int RF73<SPI,SELPIN>::receive (void* ptr, int len) {
-	if ((readReg(FIFO_STATUS) & FIFO_STATUS_RX_EMPTY) == 0) {
-		uint8_t bytes = readReg(R_RX_PL_WID_CMD);
-		if (bytes <= RF73_MAXLEN) {
-			readBuf(RD_RX_PLOAD, (uint8_t*) ptr, bytes);
+  if ((readReg(FIFO_STATUS) & FIFO_STATUS_RX_EMPTY) == 0) {
+    uint8_t bytes = readReg(R_RX_PL_WID_CMD);
+    if (bytes <= RF73_MAXLEN) {
+      readBuf(RD_RX_PLOAD, (uint8_t*) ptr, bytes);
       return bytes;
-		}
+    }
   }
   return -1;
 }
@@ -210,7 +210,7 @@ int RF73<SPI,SELPIN>::receive (void* ptr, int len) {
 template< typename SPI, int SELPIN >
 void RF73<SPI,SELPIN>::send (uint8_t ack, const void* ptr, int len) {
   const uint8_t* pbuf = (const uint8_t*) ptr;
-	txMode();
-	writeBuf(ack ? WR_TX_PLOAD : W_TX_PAYLOAD_NOACK_CMD, pbuf, len);
-	rxMode();
+  txMode();
+  writeBuf(ack ? WR_TX_PLOAD : W_TX_PAYLOAD_NOACK_CMD, pbuf, len);
+  rxMode();
 }
