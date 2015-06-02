4fe465ab5787c4a6b5d1ab9170222772a7c0fbdd
Jean-Claude Wippler <jc@wippler.nl>
2015-06-02 17:00:14 +0200

    add pins demo

---
+++ explore/1524-lpc824/pins/main.cpp
@@ -0,0 +1,30 @@
+// Demo of a C++ template wrapper for GPIO pins.
+
+#include "sys.h"
+
+template < int N >
+class Pin {
+public:
+  operator int ()             { return LPC_GPIO_PORT->B[0][N]; }
+  void operator= (int value)  { LPC_GPIO_PORT->B[0][N] = value; }
+  void setInput()             { LPC_GPIO_PORT->DIR[0] &= ~(1<<N); }
+  void setOutput()            { LPC_GPIO_PORT->DIR[0] |= 1<<N; }
+  void toggle()               { LPC_GPIO_PORT->NOT[0] = 1<<N; }
+};
+
+Pin<13> led;
+
+int main () {
+  tick.init(1000);
+
+  led.setOutput();
+  led = 1;
+  tick.delay(1000);
+  led = 0;
+  tick.delay(2000);
+
+  while (true) {
+    led.toggle();
+    tick.delay(500);
+  }
+}
