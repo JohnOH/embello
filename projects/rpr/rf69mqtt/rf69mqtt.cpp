860b83db40a7375766ba81a50ce9cffb9a9e3ebd
Jean-Claude Wippler <jc@wippler.nl>
2015-06-23 10:41:44 +0200

    re-indent

--- projects/rpr/rf69mqtt/rf69mqtt.cpp
+++ projects/rpr/rf69mqtt/rf69mqtt.cpp
@@ -38,19 +38,10 @@
 
 class MyMqtt : public mosquittopp::mosquittopp {
 public:
-  MyMqtt () : mosquittopp::mosquittopp (NAME) {
-    MyMqtt::lib_init();
-  }
-
-  virtual void on_connect (int rc) {
-    printf("connected %d\n", rc);
-  }
-
-  virtual void on_disconnect () {
-    printf("disconnected\n");
-  }
-
-  virtual void on_message (const struct mosquitto_message* msg);
+    MyMqtt () : mosquittopp::mosquittopp (NAME) { MyMqtt::lib_init(); }
+    virtual void on_connect (int rc) { printf("connected %d\n", rc); }
+    virtual void on_disconnect () { printf("disconnected\n"); }
+    virtual void on_message (const struct mosquitto_message* msg);
 };
 
 MyMqtt mqtt;
@@ -62,54 +53,54 @@ char myTopic [20];
 RF69<SpiDev0> rf;
 
 void MyMqtt::on_message (const struct mosquitto_message* msg) {
-  uint8_t hdr = (msg->payload[0] & 0x3F) | (msg->payload[1] & 0xC0);
-  rf.send(hdr, msg->payload + 2, msg->payloadlen - 2);
+    uint8_t hdr = (msg->payload[0] & 0x3F) | (msg->payload[1] & 0xC0);
+    rf.send(hdr, msg->payload + 2, msg->payloadlen - 2);
 }
 
 int main () {
-  sprintf(myTopic, "raw/%s/%d-%d", NAME, RF_FREQ, RF_GROUP);
-  printf("\n[rf69mqtt] %s @ %s\n", myTopic, SERVER);
+    sprintf(myTopic, "raw/%s/%d-%d", NAME, RF_FREQ, RF_GROUP);
+    printf("\n[rf69mqtt] %s @ %s\n", myTopic, SERVER);
 
-  wiringPiSetup();
-  if (wiringPiSPISetup (0, 4000000) < 0) {
-    printf("Can't open the SPI bus: %d\n", errno);
-    return 1;
-  }
-
-  rf.init(RF_ID, RF_GROUP, RF_FREQ);
-
-  //rf.encrypt("mysecret");
-  rf.txPower(15); // 0 = min .. 31 = max
-
-  mqtt.connect(SERVER);
-  mqtt.subscribe(0, myTopic);
-
-  struct {
-    int16_t afc;
-    uint8_t rssi;
-    uint8_t lna;
-    uint8_t buf [64];
-  } rx;
-
-  while (true) {
-    int len = rf.receive(rx.buf, sizeof rx.buf);
-    if (len >= 0) {
-#if DEBUG
-      printf("OK ");
-      for (int i = 0; i < len; ++i)
-        printf("%02x", rx.buf[i]);
-      printf(" (%d%s%d:%d)\n", rf.rssi, rf.afc < 0 ? "" : "+", rf.afc, rf.lna);
-#endif
-
-      rx.afc = rf.afc;
-      rx.rssi = rf.rssi;
-      rx.lna = rf.lna;
-
-      char topic [30];
-      sprintf(topic, "%s/%d", myTopic, rx.buf[1] & 0x3F);
-      mqtt.publish(0, topic, 4 + len, (const uint8_t*) &rx);
+    wiringPiSetup();
+    if (wiringPiSPISetup (0, 4000000) < 0) {
+        printf("Can't open the SPI bus: %d\n", errno);
+        return 1;
     }
 
-    chThdYield();
-  }
+    rf.init(RF_ID, RF_GROUP, RF_FREQ);
+    //rf.encrypt("mysecret");
+    rf.txPower(15); // 0 = min .. 31 = max
+
+    mqtt.connect(SERVER);
+    mqtt.subscribe(0, myTopic);
+
+    struct {
+        int16_t afc;
+        uint8_t rssi;
+        uint8_t lna;
+        uint8_t buf [64];
+    } rx;
+
+    while (true) {
+        int len = rf.receive(rx.buf, sizeof rx.buf);
+        if (len >= 0) {
+#if DEBUG
+            printf("OK ");
+            for (int i = 0; i < len; ++i)
+                printf("%02x", rx.buf[i]);
+            printf(" (%d%s%d:%d)\n",
+                    rf.rssi, rf.afc < 0 ? "" : "+", rf.afc, rf.lna);
+#endif
+
+            rx.afc = rf.afc;
+            rx.rssi = rf.rssi;
+            rx.lna = rf.lna;
+
+            char topic [30];
+            sprintf(topic, "%s/%d", myTopic, rx.buf[1] & 0x3F);
+            mqtt.publish(0, topic, 4 + len, (const uint8_t*) &rx);
+        }
+
+        chThdYield();
+    }
 }
