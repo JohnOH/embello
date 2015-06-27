2659c4071720d4f1b45e232de4a5df045219749d
Jean-Claude Wippler <jc@wippler.nl>
2015-06-28 00:27:32 +0200

    formatting

--- lib/arch-lpc8xx/embello.h
+++ lib/arch-lpc8xx/embello.h
@@ -57,10 +57,10 @@ class Pin {
 public:
     operator int ()             { return LPC_GPIO_PORT->B[0][N]; }
     void operator= (int value)  { LPC_GPIO_PORT->B[0][N] = value != 0; }
-    void setInput()             { LPC_GPIO_PORT->DIR[0] &= ~(1<<N); }
-    void setOutput()            { LPC_GPIO_PORT->DIR[0] |= 1<<N; }
-    void toggle()               { LPC_GPIO_PORT->NOT[0] = 1<<N; }
-    int pin()                   { return N; }
+    void setInput ()            { LPC_GPIO_PORT->DIR[0] &= ~(1<<N); }
+    void setOutput ()           { LPC_GPIO_PORT->DIR[0] |= 1<<N; }
+    void toggle ()              { LPC_GPIO_PORT->NOT[0] = 1<<N; }
+    int pin ()                  { return N; }
 };
 
 extern Tick tick;
