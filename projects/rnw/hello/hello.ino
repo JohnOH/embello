073d818dcb3ba2f987abe1413f178a1bd23ed68b
Jean-Claude Wippler <jc@wippler.nl>
2015-10-31 01:24:07 +0100

    add analog demo

--- projects/rnw/hello/hello.ino
+++ projects/rnw/hello/hello.ino
@@ -1,11 +1,11 @@
-// Send a periodic greeting over the HY-TinySTM103 serial port.
+// Periodically send the timer value to the HY-TinySTM103 serial port.
 
 void setup () {
     Serial.begin(115200);
+    Serial.print("[hello]");
 }
 
 void loop () {
-    Serial.print("hello ");
     Serial.println(millis());
     delay(1000);
 }
