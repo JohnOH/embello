0d187ef32ee75391c4703a4283a18774126a2778
Jean-Claude Wippler <jc@wippler.nl>
2016-02-23 01:57:57 +0100

    add full mapping for 16 adc channels

--- explore/1608-forth/flib/adc.fs
+++ explore/1608-forth/flib/adc.fs
@@ -17,9 +17,10 @@ $40012400 constant ADC1
 
 : adc ( pin - u )  \ read ADC value
   IMODE-ADC over io-mode!
-  \ TODO only implemented for A0..7 and B0..1
-  dup io# swap io-port if 8 + then ADC1.SQR3 !
+\ nasty way to map the pins (a "c," table offset lookup might be simpler)
+\   PA0..7 => 0..7, PB0..1 => 8..9, PC0..5 => 10..15
+  dup io# swap  io-port ?dup if shl 6 + then
+    ADC1.SQR3 !
   ADON ADC1.CR2 bis!
   begin EOC ADC1.SR bit@ until
-  ADC1.DR @
-;
+  ADC1.DR @ ;
