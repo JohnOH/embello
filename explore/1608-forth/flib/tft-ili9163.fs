0a32ac536d233f3f24af315d74325d5f81d7ca83
Jean-Claude Wippler <jc@wippler.nl>
2016-03-07 22:09:33 +0100

    try different init sequence

--- explore/1608-forth/flib/tft-ili9163.fs
+++ explore/1608-forth/flib/tft-ili9163.fs
@@ -7,13 +7,18 @@ PC6  constant TFT-RS  \ register select
 
 create tft:init
 hex
-  011 h, 205 h, 03A h, 105 h, 026 h, 104 h, 0F2 h, 101 h, 0E0 h, 13F h,
-  125 h, 11C h, 11E h, 120 h, 112 h, 12A h, 190 h, 124 h, 111 h, 100 h,
-  100 h, 100 h, 100 h, 100 h, 0E1 h, 120 h, 120 h, 120 h, 120 h, 105 h,
-  100 h, 115 h, 1A7 h, 13D h, 118 h, 125 h, 12A h, 12B h, 12B h, 13A h,
-  0B1 h, 108 h, 108 h, 0B4 h, 107 h, 0C0 h, 10A h, 102 h, 0C1 h, 102 h,
-  0C5 h, 150 h, 15B h, 0C7 h, 140 h, 02A h, 100 h, 100 h, 100 h, 17F h,
-  02B h, 100 h, 100 h, 100 h, 17F h, 036 h, 100 h, 029 h, 02C h, 0 h,
+\ 011 h, 205 h, 03A h, 105 h, 026 h, 104 h, 0F2 h, 101 h, 0E0 h, 13F h,
+\ 125 h, 11C h, 11E h, 120 h, 112 h, 12A h, 190 h, 124 h, 111 h, 100 h,
+\ 100 h, 100 h, 100 h, 100 h, 0E1 h, 120 h, 120 h, 120 h, 120 h, 105 h,
+\ 100 h, 115 h, 1A7 h, 13D h, 118 h, 125 h, 12A h, 12B h, 12B h, 13A h,
+\ 0B1 h, 108 h, 108 h, 0B4 h, 107 h, 0C0 h, 10A h, 102 h, 0C1 h, 102 h,
+\ 0C5 h, 150 h, 15B h, 0C7 h, 140 h, 02A h, 100 h, 100 h, 100 h, 17F h,
+\ 02B h, 100 h, 100 h, 100 h, 17F h, 036 h, 100 h, 029 h, 02C h, 0 h,
+
+\ https://github.com/pyrohaz/STM32F0-ILI9163/blob/master/ILI9163.c
+  001 h, 2FF h, 011 h, 214 h, 026 h, 104 h, 0C0 h, 11F h, 0C1 h, 100 h,
+  0C2 h, 100 h, 107 h, 0C3 h, 100 h, 107 h, 0C5 h, 124 h, 1C8 h, 038 h,
+  03A h, 105 h, 036 h, 108 h, 029 h, 020 h, 02C h, 0
 decimal align
 
 : >tft ( u -- )
@@ -57,6 +62,7 @@ $FC00 variable tft-fg
 : goxy ( x y -- )
   $2A >tft $100 >tft $100 or >tft $100 >tft $17F >tft
   $2B >tft $100 >tft $100 or >tft $100 >tft $17F >tft
+  $2C >tft
 ;
 
 \ clear, putpixel, and display are used by the graphics.fs code
