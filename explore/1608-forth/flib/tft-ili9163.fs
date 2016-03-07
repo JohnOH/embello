9da73921da4773a2aa76a15a262eb102c60a0d95
Jean-Claude Wippler <jc@wippler.nl>
2016-03-07 21:07:12 +0100

    completed ili9163 code, but not working yet

--- explore/1608-forth/flib/tft-ili9163.fs
+++ explore/1608-forth/flib/tft-ili9163.fs
@@ -1,5 +1,10 @@
 \ tft driver for ILI9163 chip, uses own bit-bang implementation
 
+PB12 constant TFT-CS  \ chip select
+PB13 constant TFT-SC  \ serial clock
+PB15 constant TFT-DI  \ data into LCD
+PC6  constant TFT-RS  \ register select
+
 create tft:init
 hex
   011 h, 205 h, 03A h, 105 h, 026 h, 104 h, 0F2 h, 101 h, 0E0 h, 13F h,
@@ -11,37 +16,55 @@ hex
   02B h, 100 h, 100 h, 100 h, 17F h, 036 h, 100 h, 029 h, 02C h, 0 h,
 decimal align
 
-: +tft ( u - )  \ init tft: cmd=0/data=2 + write=0/read=1
-;
-: -tft ;
+: >tft ( u -- )
+  dup $100 and TFT-RS io!
+  TFT-CS ioc!
+  8 0 do
+    dup $80 and TFT-DI io!
+    TFT-SC ios!
+    shr
+    TFT-SC ioc!
+  loop
+  TFT-CS ios!  TFT-RS ios!  drop ;
 
-: tft! ( val reg -- )
-  0 +tft 0 >spi >spi -tft
-  2 +tft dup 8 rshift >spi >spi -tft ;
+: h>tft ( u -- )
+\ assumes TFT-RS is already set
+  TFT-CS ioc!
+  16 0 do
+    dup $8000 and TFT-DI io!
+    TFT-SC ios!
+    shr
+    TFT-SC ioc!
+  loop
+  TFT-CS ios! drop ;
 
 : tft-config ( -- )
   tft:init begin
     dup h@  ?dup while
-      dup $200 and if drop $FF and ms else tft! then
+      dup $200 and if $FF and ms else >tft then
   2 + repeat drop ;
 
 $0000 variable tft-bg
 $FC00 variable tft-fg
 
 : tft-init ( -- )
-  OMODE-PP TFT-CS io-mode!
-  OMODE-PP TFT-SC io-mode!
+  OMODE-PP TFT-CS io-mode!  TFT-CS ios!
+  OMODE-PP TFT-SC io-mode!  TFT-SC ioc!
   OMODE-PP TFT-DI io-mode!
   OMODE-PP TFT-RS io-mode!
   tft-config ;
 
+: goxy ( x y -- )
+  $2A >tft $100 >tft $100 or >tft $100 >tft $17F >tft
+  $2B >tft $100 >tft $100 or >tft $100 >tft $17F >tft
+;
+
 \ clear, putpixel, and display are used by the graphics.fs code
 
 : clear ( -- )  \ clear display memory
-  0 $21 tft! 0 $20 tft!
-  tft-bg @ 320 240 * 0 do dup $22 tft! loop drop ;
+  0 0 goxy  tft-bg @  16384 0 do  dup h>tft  loop  drop ;
 
 : putpixel ( x y -- )  \ set a pixel in display memory
-  $21 tft! $20 tft! tft-fg @ $22 tft! ;
+  goxy  tft-fg @ h>tft ;
 
 : display ( -- ) ;  \ update tft from display memory (ignored)
