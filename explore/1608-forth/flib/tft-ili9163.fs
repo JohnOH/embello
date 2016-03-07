47a5bb99dab10d881269430bf6af1b9bedf443d7
Jean-Claude Wippler <jc@wippler.nl>
2016-03-07 17:08:42 +0100

    start work on ili9163 lcd

--- explore/1608-forth/flib/tft-ili9163.fs
+++ explore/1608-forth/flib/tft-ili9163.fs
@@ -1,4 +1,9 @@
-\ tft driver for ILI9325 chip
+\ tft driver for ILI9163 chip
+
+PB12 constant TFT-CS  \ chip select
+PB13 constant TFT-SC  \ serial clock
+PB15 constant TFT-DI  \ data into LCD
+PC6  constant TFT-RS  \ register select
 
 create tft:init
 hex
@@ -15,51 +20,38 @@ hex
   83 h, 0000 h, 84 h, 0000 h, 85 h, 0000 h, 90 h, 0010 h, 92 h, 0000 h,
   93 h, 0003 h, 95 h, 0110 h, 97 h, 0000 h, 98 h, 0000 h, 07 h, 0133 h,
   20 h, 0000 h, 21 h, 0000 h, 0 ,
-decimal
+
+  011 h, 205 h, 03A h, 105 h, 026 h, 104 h, 0F2 h, 101 h, 0E0 h, 13F h, 125 h,
+  11C h, 11E h, 120 h, 112 h, 12A h, 190 h, 124 h, 111 h, 100 h, 100 h, 100 h,
+  100 h, 100 h, 0E1 h, 120 h, 120 h, 120 h, 120 h, 105 h, 100 h, 115 h, 1A7 h,
+  13D h, 118 h, 125 h, 12A h, 12B h, 12B h, 13A h, 0B1 h, 108 h, 108 h, 0B4 h,
+  107 h, 0C0 h, 10A h, 102 h, 0C1 h, 102 h, 0C5 h, 150 h, 15B h, 0C7 h, 140 h,
+  02A h, 100 h, 100 h, 100 h, 17F h, 02B h, 100 h, 100 h, 100 h, 17F h, 036 h,
+  100 h, 029 h, 02C h, 0 h,
+decimal align
 
 : +tft ( u - )  \ init tft: cmd=0/data=2 + write=0/read=1
-  +spi $70 or >spi ;
-: -tft ( -- ) -spi ;
-
-: tft@ ( reg -- val )
-  0 +tft 0 >spi >spi -tft
-  3 +tft spi> 8 lshift spi> or -tft ;
+;
+: -tft ;
 
 : tft! ( val reg -- )
   0 +tft 0 >spi >spi -tft
   2 +tft dup 8 rshift >spi >spi -tft ;
 
-\ FIXME looks like SPI reads are not working...
-\ : tft. ( -- )  \ dump ILI9325 register contents
-\   cr space 16 0 do 2 spaces i h.2 loop
-\   $A0 0 do
-\     cr  i h.2 space
-\     16 0 do  i j + tft@ h.4  loop
-\   $10 +loop ;
-
 : tft-config ( -- )
   tft:init begin
-    dup @  ?dup while
-      dup 16 rshift swap ( addr val reg )
-      dup $100 and if drop ms else tft! then
-  4 + repeat drop ;
+    dup h@  ?dup while
+      dup $200 and if drop $FF and ms else tft! then
+  2 + repeat drop ;
 
 $0000 variable tft-bg
 $FC00 variable tft-fg
 
 : tft-init ( -- )
-  PB0 ssel !  \ use PB0 to select the TFT display
-  spi-init
-\ switch to alternate SPI pins, PB3..5 iso PA5..7
-  $03000001 AFIO-MAPR !  \ also disable JTAG & SWD to free PB3 PB4 PA15
-  IMODE-FLOAT PA5 io-mode!
-  IMODE-FLOAT PA6 io-mode!
-  IMODE-FLOAT PA7 io-mode!
-  OMODE-AF-PP PB3 io-mode!
-  IMODE-FLOAT PB4 io-mode!
-  OMODE-AF-PP PB5 io-mode!
-  OMODE-PP PB2 io-mode!  PB2 ios!
-  %0000000001010110 SPI1-CR1 !  \ clk/16, i.e. 4.5 MHz, master, CPOL=1 (!)
+  OMODE-PP TFT-CS io-mode!
+  OMODE-PP TFT-SC io-mode!
+  OMODE-PP TFT-DI io-mode!
+  OMODE-PP TFT-RS io-mode!
   tft-config ;
 
 \ clear, putpixel, and display are used by the graphics.fs code
