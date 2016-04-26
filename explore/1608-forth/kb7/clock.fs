a58f7d50923cc791edbb784420c2bbb2f63440f6
Jean-Claude Wippler <jc@wippler.nl>
2016-04-26 13:07:09 +0200

    remove unused code

--- explore/1608-forth/kb7/clock.fs
+++ explore/1608-forth/kb7/clock.fs
@@ -13,42 +13,6 @@
   R> not R@ @ and        \ invert bitmask and maskout new bits in current value
   or r> ! ;              \ apply value and store back
 
-: pin# ( pin -- nr ) \ get pin number from pin
-  $f and 1-foldable ;
-: port-base ( pin -- adr ) \ get port base from pin
-  $f bic 1-foldable ;
-: port# ( pin -- n ) \ return gpio port number A:0 .. K:10
-  10 rshift $f and 1-foldable ;
-: mode-mask ( pin -- m )
-  3 swap pin# 2* lshift 1-foldable ;
-: mode-shift ( mode pin -- mode<< ) \ shift mode by pin number*2 for gpio_moder
-  pin# 2* lshift 2-foldable ;
-: set-mask! ( v m a -- )
-  tuck @ swap bic rot or swap ! ;
-: bsrr-on ( pin -- v ) \ gpio_bsrr mask pin on
-  pin# 1 swap lshift 1-foldable ;
-: bsrr-off ( pin -- v ) \ gpio_bsrr mask pin off
-  pin# 16 + 1 swap lshift 1-foldable ;
-: af-mask ( pin -- mask ) \ alternate function bitmask
-  $7 and 2 lshift $f swap lshift 1-foldable ;
-: af-reg ( pin -- adr ) \ alternate function reg addr for pin
-  dup $8 and 2/ swap
-  port-base $20 + + 1-foldable ;
-: af-shift ( af pin -- af )
-  pin# 2 lshift swap lshift 2-foldable ;
-: gpio-mode! ( mode pin -- )
-  tuck mode-shift swap dup
-  mode-mask swap port-base set-mask! ;
-: mode-af ( af pin -- )
-  2 over gpio-mode!
-  dup af-mask swap af-reg bits! ;
-: speed-mode ( speed pin -- )
-  \ set speed mode 0:low speed 1:medium 2:fast 3:high speed
-  dup pin# 2* 3 swap lshift
-  swap port-base 8 + bits! ;
-: mode-af-fast ( af pin -- )
-  2 over speed-mode mode-af ;
-
 $40023C00      constant FLASH_ACR
 
 : flash-ws! ( n -- ) $f FLASH_ACR bits! ;
