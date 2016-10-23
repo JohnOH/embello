68ee5e0fb8ca0997cc2c8612ca3f3c76c0f96efb
Jean-Claude Wippler <jc@wippler.nl>
2016-10-23 23:01:18 +0200

    switch to C++, add printf and systick w/ millis()

--- explore/1649-f103/serial/main.cpp
+++ explore/1649-f103/serial/main.cpp
@@ -21,12 +21,11 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/systick.h>
 #include <stdio.h>
 #include <errno.h>
 
-int _write(int file, const char *ptr, int len);
-
-static void clock_setup (void) {
+static void clock_setup () {
     rcc_clock_setup_in_hse_8mhz_out_72mhz();
 
     /* Enable clocks for GPIO port A/C for LED and USART1. */
@@ -36,7 +35,7 @@ static void clock_setup (void) {
     rcc_periph_clock_enable(RCC_USART1);
 }
 
-static void usart_setup (void) {
+static void usart_setup () {
     /* Setup GPIO pin GPIO_USART1_RE_TX on GPIO port B for transmit. */
     gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
               GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
@@ -57,14 +56,7 @@ static void usart_setup (void) {
     usart_enable(USART1);
 }
 
-static void leds_setup (void) {
-    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
-              GPIO_CNF_OUTPUT_PUSHPULL, GPIO1);
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-              GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
-}
-
-int _write (int file, const char *ptr, int len) {
+extern "C" int _write (int file, const char *ptr, int len) {
     if (file == 1) {
         for (int i = 0; i < len; ++i)
             usart_send_blocking(USART1, ptr[i]);
@@ -74,28 +66,48 @@ int _write (int file, const char *ptr, int len) {
     return -1;
 }
 
-static int serial_getc (void) {
+int serial_getc () {
     return USART_SR(USART1) & USART_SR_RXNE ? usart_recv(USART1) : -1;
 }
 
-int main (void) {
+static void systick_setup () {
+    /* 72MHz / 8 => 9000000 counts per second. */
+    systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
+
+    /* 9000000/9000 = 1000 overflows per second - every 1ms one interrupt */
+    /* SysTick interrupt every N clock pulses: set reload to N-1 */
+    systick_set_reload(8999);
+
+    systick_interrupt_enable();
+
+    /* Start counting. */
+    systick_counter_enable();
+}
+
+static uint32_t ticks;
+
+extern "C" void sys_tick_handler () {
+    ++ticks;
+}
+
+uint32_t millis () {
+    return ticks;
+}
+
+// the following creates an Arduino-like environment with setup() and loop()
+
+extern void setup ();
+extern void loop ();
+
+int main () {
     clock_setup();
     usart_setup();
-    leds_setup();
+    systick_setup();
 
-    const char msg[] = "\r\nHit <enter> to toggle the LED... ";
-    _write(1, msg, sizeof msg - 1);
+    setup();
 
-    while (true) {
-        _write(1, msg, sizeof msg - 1);
+    while (true)
+        loop();
 
-        for (int i = 0; i < 1000000; ++i) {
-            if (serial_getc() == '\r') {
-                gpio_toggle(GPIOA, GPIO1);
-                gpio_toggle(GPIOC, GPIO13);
-            }
-        }
-    }
-
-    return 0;
+    // never returns
 }
