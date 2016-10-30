4733b4f210130f3e6e25ca0ab7fbd41a59ab80e3
Jean-Claude Wippler <jc@wippler.nl>
2016-10-30 18:04:00 +0100

    cleanup

--- explore/1608-forth/jnl/e-adc.fs
+++ explore/1608-forth/jnl/e-adc.fs
@@ -2,7 +2,7 @@
 \ needs core.fs
 cr cr reset
 
-include ../flib/adc-stm32l0.fs
+\ include ../flib/adc-stm32l0.fs
 
 \ pin definitions: PA0 = 0V, PA1 = ADC in, PA2 = 3.3V
 \ works well for a trimpot inserted in a breadboard
@@ -10,21 +10,20 @@ include ../flib/adc-stm32l0.fs
 imode-adc pa1 io-mode!
  omode-pp pa2 io-mode!  pa2 ios!
 
++adc adc.
+
+: temp+vcc
+  cr micros adc-temp micros rot - . ." µs " . ." °C "
+  cr micros adc-vcc micros rot - . ." µs " . ." mV "
+;
+
+temp+vcc
+
 : go
-  +adc adc.
   begin
     cr micros pa1 adc micros rot - . .
     500 ms
   key? until ;
 
 \ this causes folie to timeout on include matching, yet still starts running
-\ 1234 ms go
-
-+adc 
-
-: go
-  cr micros adc-temp micros rot - . ." µs " . ." °C "
-  cr micros adc-vcc micros rot - . ." µs " . ." mV "
-;
-
-go
+1234 ms go
