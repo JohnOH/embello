e57832b1951a18922314d60b669bf4b1f0354045
Jean-Claude Wippler <jc@wippler.nl>
2016-11-14 23:12:15 +0100

    explore a somewhat different I2C API

--- explore/1608-forth/i2c/e-veml.fs
+++ explore/1608-forth/i2c/e-veml.fs
@@ -2,20 +2,67 @@
 \ needs core.fs
 cr cr reset
 
-include ../flib/i2c-stm32l0.fs
+\ include ../flib/i2c-stm32l0.fs
 \ include ../flib/veml6040.fs
+
+100 buffer: i2c.buf
+ 0 variable i2c.ptr
+ 0 variable i2c.hdr
+
+: i2c-reset ( -- )  i2c.buf i2c.ptr ! ;
+
+: i2c-addr ( u -- )  shl i2c.hdr c!  i2c-reset ;
+
+: i2c++ ( -- addr )  i2c.ptr @  1 i2c.ptr +! ;
+
+: >i2c ( u -- ) i2c++ c! ;
+: i2c> ( -- u ) i2c++ c@ ;
+
+: n>i2c ( u -- )
+  drop
+;
+
+: i2c>n ( u -- )
+  drop
+;
+
+: i2c-txrx ;
+: i2c-tx ;
+: i2c-rx ;
+
+\ there are 4 cases:
+\   tx>0 rx>0 : START - tx - RESTART - rx - STOP
+\   tx>0 rx=0 : START - tx - STOP
+\   tx=0 rx>0 : START - rx - STOP
+\   tx=0 rx=0 : START - STOP          (used for presence detection)
+
+: i2c-xfer ( u -- f )
+  i2c.ptr @ i2c.buf - ?dup if
+    n>i2c
+    ?dup if
+      i2c-txrx \ tx>0 rx>0
+    else
+      i2c-tx \ tx>0 rx=0
+    then
+  else
+    ?dup if
+      i2c-rx \ tx=0 rx>0
+    else
+      i2c-tx \ tx=0 rx=0
+    then
+  then
+  i2c-reset ;
 
 \ assumes that the VEML6040 sensor is connected to PB6..PB7
 
 : veml-init ( -- )
-  $10 i2c-tx drop  3 0 do  $00 >i2c drop  loop  i2c-stop ;
+  $10 i2c-addr  3 0 do $00 >i2c loop  0 i2c-xfer drop ;
 
 : veml-rd ( reg -- val )
-  $10 i2c-tx drop >i2c drop i2c-stop
-  $10 i2c-rx drop 0 i2c> 1 i2c> i2c-stop
-  8 lshift or ;
+  $10 i2c-addr  >i2c  2 i2c-xfer drop
+  i2c> i2c> 8 lshift or ;
 
-: veml-data $08 veml-rd $09 veml-rd $0A veml-rd $0B veml-rd ;
+: veml-data $8 veml-rd $9 veml-rd $A veml-rd $B veml-rd ;
 
 : go
   veml-init
@@ -27,6 +74,7 @@ include ../flib/i2c-stm32l0.fs
   key? until ;
 
 +i2c 100 ms i2c? \ i2c.
+i2c.buf 100 0 fill
 
 \ this causes folie to timeout on include matching, yet still starts running
 1234 ms go
