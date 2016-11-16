d43b32df0075de4202e0efeaa8a6008150277c9e
Jean-Claude Wippler <jc@wippler.nl>
2016-11-16 14:44:29 +0100

    move the VEML6040 driver to its own file

--- explore/1608-forth/i2c/e-veml.fs
+++ explore/1608-forth/i2c/e-veml.fs
@@ -2,19 +2,10 @@
 \ needs core.fs
 cr cr reset
 
-include ../flib/i2c-stm32l0.fs
-\ include ../flib/veml6040.fs
+\ include ../flib/i2c-stm32l0.fs
+include ../flib/veml6040.fs
 
 \ assumes that the VEML6040 sensor is connected to PB6..PB7
-
-: veml-init ( -- )
-  +i2c  $10 i2c-addr  3 0 do $00 >i2c loop  0 i2c-xfer drop ;
-
-: veml-rd ( reg -- val )
-  $10 i2c-addr  >i2c  2 i2c-xfer drop
-  i2c> i2c> 8 lshift or ;
-
-: veml-data $8 veml-rd $9 veml-rd $A veml-rd $B veml-rd ;
 
 : go
   veml-init
