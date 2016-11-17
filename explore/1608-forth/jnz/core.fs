5c1b3e0e833b11ed8494720b66df1d7394725d29
Jean-Claude Wippler <jc@wippler.nl>
2016-11-17 23:26:07 +0100

    add new jnz rev3 pinout

---
+++ explore/1608-forth/jnz/core.fs
@@ -0,0 +1,17 @@
+\ core libraries
+
+<<<board>>>
+cr compiletoflash
+( core start: ) here dup hex.
+
+include ../flib/rf69.fs
+include ../flib/varint.fs
+include ../flib/bme280.fs
+include ../flib/veml6040.fs
+include ../flib/mag3110.fs
+include ../flib/oled.fs
+include ../mlib/graphics.fs
+include ../mlib/multi.fs
+
+( core end, size: ) here dup hex. swap - .
+cornerstone <<<core>>>
---
+++ explore/1608-forth/jnz/main.fs
@@ -0,0 +1,45 @@
+\ application setup and main loop
+\ assumes that the BME280 and TSL4531 sensors are connected to PB6..PB7
+
+0 constant debug  \ 0 = send RF packets, 1 = display on serial port
+10 constant rate  \ seconds between readings
+
+: show-readings ( vprev vcc tint lux humi pres temp -- )
+  hwid hex. ." = "
+  . ." °cC, " . ." Pa, " . ." %cRH, "
+  . ." lux, "  . ." °C, " . ." => " . ." mV " ;
+
+: send-packet ( vprev vcc tint lux humi pres temp -- )
+  2 <pkt  hwid u+>  u14+> 6 0 do u+> loop  pkt>rf ;
+
+: low-power-sleep
+  rf-sleep
+  -adc \ only-msi
+  rate 0 do stop1s loop
+  hsi-on +adc ;
+
+: main
+  2.1MHz  1000 systick-hz  +lptim +i2c +adc
+
+  8686 rf69.freq ! 6 rf69.group ! 62 rf69.nodeid !
+  rf69-init 16 rf-power
+
+  bme-init drop bme-calib
+  tsl-init drop
+
+  begin
+    led-off 
+
+    adc-vcc                      ( vprev )
+    low-power-sleep
+    adc-vcc adc-temp             ( vprev vcc tint )
+    tsl-data  bme-data bme-calc  ( vprev vcc tint lux humi pres temp )
+
+    led-on
+
+    debug if
+      show-readings cr 1 ms
+    else
+      send-packet
+    then
+  key? until ;
