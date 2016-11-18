a87567049352c3d6772547366dfef0b7921a712b
Jean-Claude Wippler <jc@wippler.nl>
2016-11-18 01:34:56 +0100

    more stop mode savings

--- explore/1608-forth/jz1/e-stop.fs
+++ explore/1608-forth/jz1/e-stop.fs
@@ -10,16 +10,41 @@ include ../flib/sleep-stm32l0.fs
      $40015804 constant DBG-CR
 
 : lp-blink ( -- )
-  2.1MHz 1000 systick-hz
+  2.1MHz  1000 systick-hz
 \ 11 bit PWR-CR bis!                  \ 1.2V, range 3
 \ begin 4 bit PWR-CSR bit@ not until  \ wait for !VOSF
   begin
+    stop1s stop1s stop1s stop1s stop1s
     stop1s stop1s stop1s stop1s stop1s
     led-on 10 ms led-off
   again ;
 
 [IFDEF] rf69-init  rf69-init rf-sleep  [THEN]
 +lptim lptim?
+
+\ this s(h)aves 0.4 µA
+IMODE-ADC PA0  io-mode!
+IMODE-ADC PA1  io-mode!
+IMODE-ADC PA2  io-mode!
+IMODE-ADC PA3  io-mode!
+IMODE-ADC PA4  io-mode!
+IMODE-ADC PA5  io-mode!
+IMODE-ADC PA6  io-mode!
+IMODE-ADC PA7  io-mode!
+IMODE-ADC PA8  io-mode!
+IMODE-ADC PA11 io-mode!
+IMODE-ADC PA12 io-mode!
+IMODE-ADC PA13 io-mode!
+IMODE-ADC PA14 io-mode!
+IMODE-ADC PB0  io-mode!
+IMODE-ADC PB1  io-mode!
+IMODE-ADC PB3  io-mode!
+IMODE-ADC PB4  io-mode!
+IMODE-ADC PB5  io-mode!
+IMODE-ADC PB6  io-mode!
+IMODE-ADC PB7  io-mode!
+IMODE-ADC PC14 io-mode!
+IMODE-ADC PC15 io-mode!
 
 ( clock-hz    ) clock-hz @ .
 ( PWR-CR      ) PWR-CR @ hex.
