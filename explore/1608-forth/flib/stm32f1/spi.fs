8f0484609599d1954b76019106ec4509d4c9edda
Jean-Claude Wippler <jc@wippler.nl>
2016-11-21 18:28:42 +0100

    add 32- and 36-pin package definitions

--- explore/1608-forth/flib/stm32f1/spi.fs
+++ explore/1608-forth/flib/stm32f1/spi.fs
@@ -1,7 +1,6 @@
 \ hardware SPI driver
 
-PA4  variable ssel   \ can be changed at run time
-PB12 variable ssel2  \ can be changed at run time
+PA4 variable ssel   \ can be changed at run time
 
 $40013000 constant SPI1
      SPI1 $0 + constant SPI1-CR1
@@ -33,36 +32,4 @@ $40013000 constant SPI1
   %0000000001010100 SPI1-CR1 !  \ clk/8, i.e. 9 MHz, master
   SPI1-SR @ drop  \ appears to be needed to avoid hang in some cases
   2 bit SPI1-CR2 bis!  \ SS output enable
-;
-
-$40003800 constant SPI2
-     SPI2 $0 + constant SPI2-CR1
-     SPI2 $4 + constant SPI2-CR2
-     SPI2 $8 + constant SPI2-SR
-     SPI2 $C + constant SPI2-DR
-
-: spi2. ( -- )  \ display SPI hardware registers
-  cr ." CR1 " SPI2-CR1 @ h.4
-    ."  CR2 " SPI2-CR2 @ h.4
-     ."  SR " SPI2-SR @ h.4 ;
-
-: +spi2 ( -- ) ssel2 @ ioc! ;  \ select SPI
-: -spi2 ( -- ) ssel2 @ ios! ;  \ deselect SPI
-
-: >spi2> ( c -- c )  \ hardware SPI, 8 bits
-  SPI2-DR !  begin SPI2-SR @ 1 and until  SPI2-DR @ ;
-
-\ single byte transfers
-: spi2> ( -- c ) 0 >spi2> ;  \ read byte from SPI
-: >spi2 ( c -- ) >spi2> drop ;  \ write byte to SPI
-
-: spi2-init ( -- )  \ set up hardware SPI
-  OMODE-PP ssel2 @ io-mode! -spi2
-  OMODE-AF-PP PB13 io-mode!
-  IMODE-FLOAT PB14 io-mode!
-  OMODE-AF-PP PB15 io-mode!
-  14 bit RCC-APB1ENR bis!  \ set SPI2EN
-  %0000000001001100 SPI2-CR1 !  \ clk/4, i.e. 9 MHz, master
-  SPI2-SR @ drop  \ appears to be needed to avoid hang in some cases
-  2 bit SPI2-CR2 bis!  \ SS output enable
 ;
--- explore/1608-forth/flib/stm32f7/hal.fs
+++ explore/1608-forth/flib/stm32f7/hal.fs
@@ -15,42 +15,6 @@
 : io.all ( -- )  \ display all the readable GPIO registers
   io-ports 0 do i 0 io io. loop ;
 
-\ define pins for STM32F746VG, LQFP-100 package
-
-0 0  io constant PA0      1 0  io constant PB0      2 0  io constant PC0
-0 1  io constant PA1      1 1  io constant PB1      2 1  io constant PC1
-0 2  io constant PA2      1 2  io constant PB2      2 2  io constant PC2
-0 3  io constant PA3      1 3  io constant PB3      2 3  io constant PC3
-0 4  io constant PA4      1 4  io constant PB4      2 4  io constant PC4
-0 5  io constant PA5      1 5  io constant PB5      2 5  io constant PC5
-0 6  io constant PA6      1 6  io constant PB6      2 6  io constant PC6
-0 7  io constant PA7      1 7  io constant PB7      2 7  io constant PC7
-0 8  io constant PA8      1 8  io constant PB8      2 8  io constant PC8
-0 9  io constant PA9      1 9  io constant PB9      2 9  io constant PC9
-0 10 io constant PA10     1 10 io constant PB10     2 10 io constant PC10
-0 11 io constant PA11     1 11 io constant PB11     2 11 io constant PC11
-0 12 io constant PA12     1 12 io constant PB12     2 12 io constant PC12
-0 13 io constant PA13     1 13 io constant PB13     2 13 io constant PC13
-0 14 io constant PA14     1 14 io constant PB14     2 14 io constant PC14
-0 15 io constant PA15     1 15 io constant PB15     2 15 io constant PC15
-
-3 0  io constant PD0      4 0  io constant PE0      7 0  io constant PH0
-3 1  io constant PD1      4 1  io constant PE1      7 1  io constant PH1
-3 2  io constant PD2      4 2  io constant PE2
-3 3  io constant PD3      4 3  io constant PE3
-3 4  io constant PD4      4 4  io constant PE4
-3 5  io constant PD5      4 5  io constant PE5
-3 6  io constant PD6      4 6  io constant PE6
-3 7  io constant PD7      4 7  io constant PE7
-3 8  io constant PD8      4 8  io constant PE8
-3 9  io constant PD9      4 9  io constant PE9
-3 10 io constant PD10     4 10 io constant PE10
-3 11 io constant PD11     4 11 io constant PE11
-3 12 io constant PD12     4 12 io constant PE12
-3 13 io constant PD13     4 13 io constant PE13
-3 14 io constant PD14     4 14 io constant PE14
-3 15 io constant PD15     4 15 io constant PE15
-
 $40010000 constant AFIO
      AFIO $4 + constant AFIO-MAPR
 
