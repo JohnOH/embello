fe1658c1228623cfb6885a830622f65badd78827
Jean-Claude Wippler <jc@wippler.nl>
2016-12-13 16:34:04 +0100

    fix olimexino and maplemini usb images

--- explore/1608-forth/suf/f-olimexino.fs
+++ explore/1608-forth/suf/f-olimexino.fs
@@ -8,13 +8,16 @@ include ../flib/any/ring.fs
 include usb.fs
 
 : init ( -- )
-\ 1000000 0 do loop  \ approx 1s delay
+  $3D RCC-APB2ENR !  \ enable AFIO and GPIOA..D clocks
   72MHz  \ this is required for USB use
-\ key? if key if exit then then  \ safety escape hatch
+
   \ board-specific way to enable USB
   %1111 16 lshift $40011004 bic!  \ PC12: output, push-pull, 2 MHz
   %0010 16 lshift $40011004 bis!  \ ... this affects CRH iso CRL
+  12 bit $4001100C bis!  \ set PC12 high
+  100000 0 do loop
   12 bit $4001100C bic!  \ set PC12 low
+
   usb-io  \ switch to USB as console
 ;
 
