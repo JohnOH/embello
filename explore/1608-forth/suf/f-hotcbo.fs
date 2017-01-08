c6ba5942e8b2d0c4941d3f636a508d780fe13b0f
Jean-Claude Wippler <jc@wippler.nl>
2017-01-08 23:46:55 +0100

    add USB builds for two more F103 boards

---
+++ explore/1608-forth/suf/f-hotcbo.fs
@@ -0,0 +1,26 @@
+\ USB console for HotMCU Core Board 1xx
+
+$5000 eraseflashfrom  \ this must be loaded on top of a *clean* Mecrisp image!
+cr compiletoflash
+
+include hal-stm32f1.fs
+include ../flib/any/ring.fs
+include usb.fs
+
+: init ( -- )
+  $FD RCC-APB2ENR !  \ enable AFIO and GPIOA..F clocks
+  72MHz  \ this is required for USB use
+
+  \ board-specific way to enable USB
+  %1111 8 lshift $40011C04 bic!  \ PF10: output, push-pull, 2 MHz
+  %0010 8 lshift $40011C04 bis!  \ ... this affects CRH iso CRL
+  10 bit $40011C0C bis!  \ set PF10 high
+  100000 0 do loop
+  10 bit $40011C0C bic!  \ set PF10 low
+
+  usb-io  \ switch to USB as console
+;
+
+( usb end: ) here hex.
+cornerstone eraseflash
+compiletoram
---
+++ explore/1608-forth/suf/f-olip103.fs
@@ -0,0 +1,26 @@
+\ USB console for Olimex P103 board
+
+$5000 eraseflashfrom  \ this must be loaded on top of a *clean* Mecrisp image!
+cr compiletoflash
+
+include hal-stm32f1.fs
+include ../flib/any/ring.fs
+include usb.fs
+
+: init ( -- )
+  $3D RCC-APB2ENR !  \ enable AFIO and GPIOA..D clocks
+  72MHz  \ this is required for USB use
+
+  \ board-specific way to enable USB
+  %1111 12 lshift $40011004 bic!  \ PC11: output, push-pull, 2 MHz
+  %0010 12 lshift $40011004 bis!  \ ... this affects CRH iso CRL
+  11 bit $4001100C bis!  \ set PC11 high
+  100000 0 do loop
+  11 bit $4001100C bic!  \ set PC11 low
+
+  usb-io  \ switch to USB as console
+;
+
+( usb end: ) here hex.
+cornerstone eraseflash
+compiletoram
