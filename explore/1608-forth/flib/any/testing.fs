e9b91373be372bcae3b9e72649dc4e60371de491
Thorsten von Eicken <tve@voneicken.com>
2017-02-06 18:43:15 -0800

    switch varint to signed 32-bit integers

---
+++ explore/1608-forth/flib/any/testing.fs
@@ -0,0 +1,33 @@
+\ Simple support for unit tests. In the main program, define:
+\ 1 constant TESTS
+\ to enable tests and wrap unit tests like
+\ [ifdef] TESTS
+\   ...tests...
+\ [then]
+
+[ifdef] TESTS
+
+  -1 variable tests-OK
+  : fail-tests 0 tests-OK ! ;
+  : test-summary 
+    tests-OK @ not if ." ** TESTS FAILED! **" else
+    depth 0<> if ." ** TESTS OK but stack not empty: " .v else
+    ." ** ALL OK **" then then cr ;
+
+  : =always ( n1 n2 -- ) \ assert that the two TOS values must be equal
+    2dup <> if
+      ." FAIL: got " swap . ." expected " . fail-tests
+    else 2drop then ;
+
+  : =always-fix ( df1 df2 -- ) \ assert that the two TOS fixed-point values must be equal
+    2dup 2rot 2dup 2rot ( df2 df1 df1 df2 )
+    d<> if
+      ." FAIL: got " f. ." expected " f. fail-tests
+    else 2drop 2drop then ;
+
+  : always ( f -- )
+    0= if
+      ." FAIL!" fail-tests
+    else ." OK!" then ;
+
+[then]
--- explore/1608-forth/flib/any/varint.fs
+++ explore/1608-forth/flib/any/varint.fs
@@ -1,40 +1,55 @@
-\ variable int encoding for RF packet use
-\ needs rf69.h
+\ variable int encoding for use in RF packets. Encodes up to 32-bit integers using
+\ a variable-length encoding of 1..5 bytes. All values are encoded as signed
+\ integers, which means that if an unsigned value is encoded the recipient needs
+\ to know the integer's size in order to be able to cast it from int to uint.
+\ For example, the unsigned 32-bit int 0xffffffff is encoded as if it was the
+\ signed value -1. At the receiving end it will be decoded as -1, but as long as the
+\ receiver knows that this is a 32-bit value it can be type-cast to uint32 to recover
+\ 0xffffffff.
+\ 
+\ The encoding is as follows:
+\ - the int is rotated 1 bit left placing the sign bit in the lowest bit position
+\ - if bit 0 (now sign bit) is 1 (negative) all other bits are inverted
+\ - group bits in the integer in 7-bit groups, emit a byte for each 7-bit group
+\   starting with the highest non-zero group
+\ - when emitting the last byte for the lowest bits, set the top bit of the byte
+\ - the value zero encodes to 0x80
+\ 
+\ Dependency: needs rf-send to be defined, e.g. from rf69.h
 
-\ variable-int encoding, turns 64-bit ints into 1..10 bytes
-: drshift ( ud|d n -- ud|d )  \ double right shift n bits
-  0 do dshr loop ;
+\ Definitions to emit 32-bit ints as varints into the print buffer
 
-: <v ( - d ) 0 0 <# ;  \ prepare variable output
-: d#v ( d -- )  \ add one 64-bit value to output
-  over $80 or hold
+: <v ( -- ) <# ;  \ prepare variable output
+: v> ( -- caddr len ) 0 0 #> ;  \ finish, then return buffer and length
+
+: >var ( n -- )  \ add one 32-bit value to output
+  \ shift one position left - if negative, invert all bits (puts sign in bit 0)
+  \ this compresses better for *signed* values of small magnitude
+  rol dup 1 and 0<> shl xor
+  \ output lowest 7 bits
+  dup $80 or hold
+  \ output higher 7-bit groups
   begin
-    7 drshift
-  2dup or while
-    over $7F and hold
-  repeat 2drop ;
-: v> ( d -- caddr len ) #> ;  \ finish, then return buffer and length
-: u#v ( u -- ) 0 d#v ;  \ add a 32-bit uint to output as varint, max 5 bytes
+    7 rshift
+  dup while
+    dup $7F and hold
+  repeat drop ;
 
-\ some definitions to build up and send a packet with varints
+\ some definitions to build up and send a packet with varints.
+\ for example, to send a packet of type 123, with values 11, 2222, and -3333:
+\   123 <pkt 11 n+> 2222 n+> -3333 n+> pkt>rf
 
 20 cells buffer: pkt.buf  \ room to collect up to 20 values for sending
       0 variable pkt.ptr  \ current position in this packet buffer
 
-: u+> ( u -- ) pkt.ptr @ ! 4 pkt.ptr +! ;  \ append 32-bit value to packet
+: >+pkt ( n -- ) pkt.ptr @ ! 4 pkt.ptr +! ;  \ append 32-bit signed value to packet
 
-\ shift one position left - if negative, invert all bits (puts sign in bit 0)
-\ this compresses better for *signed* values of small magnitude
-: n+> ( n -- ) shl  dup 0< xor  u+> ;      \ append signed value to packet
-
-: <pkt ( format -- ) pkt.buf pkt.ptr ! u+> ;  \ start collecting values
+: <pkt ( format -- ) pkt.buf pkt.ptr ! >+pkt ;  \ start collecting values
 : pkt>rf ( -- )  \ broadcast the collected values as RF packet
   <v
-    pkt.ptr @  begin  4 - dup @ u#v  dup pkt.buf u<= until  drop
+    pkt.ptr @  begin  4 - dup @ >var  dup pkt.buf u<= until  drop
   v> 0 rf-send ;
 
-\ for example, to send a packet of type 123, with values 11, 2222, and 333333:
-\   123 <pkt 11 u+> 2222 u+> 333333 u+> pkt>rf
 : *++ ( addr -- c )  dup @ c@  1 rot +! ;
 
 \ variable-int decoding: call var-init once, then var> until it returns 0
@@ -46,14 +61,85 @@
 : var-init ( addr cnt -- )
   over + var.end ! var.ptr ! ;
 
-: var> ( -- 0 | n 1 )
+: var> ( -- 0 | n 1 ) \ extract a signed number from the var buffer
   0
   var.ptr @ var.end @ u< if 
     begin
       7 lshift  var.ptr *++  tuck + swap
     $80 and until
-    $80 - 1
+    $80 -
+    dup 1 and 0<> shl xor ror \ handle sign
+    1
   then ;
 
 : var. ( addr cnt -- )  \ decode and display all the varints
   var-init begin var> while . repeat ;
+
+[ifdef] TESTS
+
+  8 buffer: test-buf
+  8 buffer: test-buf2
+
+  : test:>var ( n b1 b2 ... bi i -- )
+    test-buf stack>buffer rot
+    ." Testing " dup .
+    <v >var v> test-buf2 -rot buffer-cpy
+    \ ." got: " 2dup buffer. cr
+    \ ." exp: " 2over buffer. cr
+    compare always
+    ;
+
+  0   $80 1 test:>var
+  1   $82 1 test:>var
+  2   $84 1 test:>var
+  -1  $81 1 test:>var
+  -2  $83 1 test:>var
+
+  \ test positive numbers
+  63  $fe 1 test:>var
+  64  1 $80 2 test:>var
+  127 1 $fe 2 test:>var
+  12   6 lshift 34 +  12 196 2 test:>var
+  12  13 lshift 34 +  12 0 128 68 + 3 test:>var
+  $7f 24 lshift       $0f $70 0 0 128 5 test:>var
+
+  \ test negative numbers
+  -64  $ff 1 test:>var
+  -65  1 $81 2 test:>var
+  -127 1 $fd 2 test:>var
+  -128 1 $ff 2 test:>var
+  12   6 lshift negate 34 +  11 187 2 test:>var
+  12  13 lshift negate 34 +  11 $7f 187 3 test:>var
+  $80000000                  $0f $7f $7f $7f $ff 5 test:>var
+
+  : test:var> ( n b1 b2 ... bi i -- )
+    test-buf stack>buffer 
+    ." Testing " 2dup buffer.
+    var-init var> always
+    =always
+    ;
+
+  0   $80 1 test:var>
+  1   $82 1 test:var>
+  2   $84 1 test:var>
+  -1  $81 1 test:var>
+  -2  $83 1 test:var>
+
+  \ test positive numbers
+  63  $fe 1 test:var>
+  64  1 $80 2 test:var>
+  127 1 $fe 2 test:var>
+  12   6 lshift 34 +  12 196 2 test:var>
+  12  13 lshift 34 +  12 0 128 68 + 3 test:var>
+  $7f 24 lshift       $0f $70 0 0 128 5 test:var>
+
+  \ test negative numbers
+  -64  $ff 1 test:var>
+  -65  1 $81 2 test:var>
+  -127 1 $fd 2 test:var>
+  -128 1 $ff 2 test:var>
+  12   6 lshift negate 34 +  11 187 2 test:var>
+  12  13 lshift negate 34 +  11 $7f 187 3 test:var>
+  $80000000                  $0f $7f $7f $7f $ff 5 test:var>
+
+[then]
