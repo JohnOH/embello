d58f59d2f029bfda0a7cbef2bd9dc1f0916fe8e6
Jean-Claude Wippler <jc@wippler.nl>
2017-03-23 10:11:06 +0100

    cleanup

--- explore/1608-forth/g6s/ex/timers.fs
+++ explore/1608-forth/g6s/ex/timers.fs
@@ -15,21 +15,10 @@ PB2  constant LED4
   OMODE-PP LED3 io-mode!
   OMODE-PP LED4 io-mode! ;
 
-: blink1 ( -- )
-    LED1 iox!   \ toggle LED1
-  ;
-
-: blink2 ( -- )
-    LED2 iox!   \ toggle LED2
-  ;
-
-: blink3 ( -- )
-    LED3 iox!   \ toggle LED3
-  ;
-
-: blink4 ( -- )
-    LED4 iox!   \ toggle LED4
-  ;
+: blink1 ( -- )  LED1 iox! ;   \ toggle LED1
+: blink2 ( -- )  LED2 iox! ;   \ toggle LED2
+: blink3 ( -- )  LED3 iox! ;   \ toggle LED3
+: blink4 ( -- )  LED4 iox! ;   \ toggle LED4
 
 setup
 timed-init
