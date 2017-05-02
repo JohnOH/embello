9942c6d8b8744eec4ede3f775ce50e41171c7724
Jean-Claude Wippler <jc@wippler.nl>
2017-05-02 20:31:02 +0200

    tweak

--- explore/1608-forth/g6s/core.fs
+++ explore/1608-forth/g6s/core.fs
@@ -1,13 +1,14 @@
 \ core definitions
 
 <<<board>>>
-hello
 compiletoflash
+( core start: ) here hex.
 
 include ../flib/i2c/ssd1306.fs
 include ../flib/mecrisp/graphics.fs
 include ../flib/any/digits.fs
 include ../flib/mecrisp/multi.fs
+include ../flib/any/timed.fs
 
 cornerstone <<<core>>>
 hello
