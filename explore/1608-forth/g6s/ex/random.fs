a810037d8c2b67f275777730b2e765c23d8ab584
Jean-Claude Wippler <jc@wippler.nl>
2017-05-08 15:43:51 +0200

    tweaks

--- explore/1608-forth/g6s/ex/random.fs
+++ explore/1608-forth/g6s/ex/random.fs
@@ -1,4 +1,4 @@
-\ try out the pseudo-random generator
+\ try out the pseudo-random number generator
 \
 \ sample output:
 \   2000000 tally counts
@@ -18,21 +18,39 @@
 \   1000394
 \   1000672
 \   999687  ok.
+\
+\   time 542006  ok.
 
 forgetram
 
 include ../../flib/mecrisp/random.fs
 
 16 cells buffer: buckets
-buckets 16 cells 0 fill
 
-: tally ( n -- )
+: ++count ( n -- )  cells buckets +  1 swap +! ;
+
+: tally ( n -- )  \ count all the 4-bit nibbles of N 32-bit random numbers
+  buckets 16 cells 0 fill
   0 do
     random
-    32 0 do 1 over i rshift $F and cells buckets + +! 4 +loop
+    32 0 do
+      dup i rshift $F and
+      ++count
+    4 +loop
     drop
   loop ;
 
-: counts 16 0 do i cells buckets + @ cr . loop ;
+: tally2 ( n -- )  \ count N random numbers in the range 0..15
+  buckets 16 cells 0 fill
+  0 do
+    16 randrange ++count
+  loop ;
+
+: counts ( -- )  \ display the 16 aggregated counts
+  16 0 do i cells buckets + @ cr . loop ;
+
+: time micros 1000000 0 do random drop loop micros swap - . ;
 
 \ 2000000 tally counts
+\ 16000000 tally2 counts
+\ time
