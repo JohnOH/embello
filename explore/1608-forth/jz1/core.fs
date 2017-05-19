acbd1522ab09a8237b637e83e06ed4befe4eb0aa
Jean-Claude Wippler <jc@wippler.nl>
2017-05-19 15:32:01 +0200

    start on rf73 driver, simple recv working

--- explore/1608-forth/jz1/core.fs
+++ explore/1608-forth/jz1/core.fs
@@ -4,14 +4,14 @@
 compiletoflash
 ( core start: ) here dup hex.
 
-include ../flib/spi/rf69.fs
-include ../flib/any/varint.fs
-include ../flib/i2c/bme280.fs
-include ../flib/i2c/tsl4531.fs
-include ../flib/i2c/veml6040.fs
-include ../flib/i2c/mag3110.fs
-include ../flib/i2c/ssd1306.fs
-include ../flib/mecrisp/graphics.fs
+\ include ../flib/spi/rf69.fs
+\ include ../flib/any/varint.fs
+\ include ../flib/i2c/bme280.fs
+\ include ../flib/i2c/tsl4531.fs
+\ include ../flib/i2c/veml6040.fs
+\ include ../flib/i2c/mag3110.fs
+\ include ../flib/i2c/ssd1306.fs
+\ include ../flib/mecrisp/graphics.fs
 include ../flib/mecrisp/multi.fs
 
 ( core end, size: ) here dup hex. swap - .
---
+++ explore/1608-forth/jz1/ex/recv70.fs
@@ -0,0 +1,8 @@
+\ try out RF70 reception
+
+forgetram
+include ../../flib/spi/rf73.fs
+
+rf-init
+rf.
+rf-listen
